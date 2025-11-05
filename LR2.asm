72ae0a4ccd0af7832a73091b7a3a942c3477362a
Anthony Timoshin <95761331+anthonytimoshin@users.noreply.github.com>
2025-11-05 18:53:44 +0300

    Update LR2.asm

--- LR2.asm
+++ LR2.asm
@@ -2,53 +2,84 @@ ORG 100h
 
 START: 
     MOV SI, [CEIL]
-    MOV BX, [FLOOR] ; BX = base (floor) value of X  
-   
-Y1:
-    MOV AX, [D]
-    MUL AX          ; D^2
-    ADD AX, BX      ; D^2+X
-    IMUL [B]        ; (D^2+X)*B
-    CALL PRINT  
-    CALL INCREASE
+    MOV BX, [FLOOR] ; BX = base (floor) value of X 
 
-Y2:
-    MOV AX, [C]      
-    ADD AX, [B]     ; C+B
-    CWD             ; AX -> DX:AX
-    IDIV [D]        ; (C+B)/D
-    CALL PRINT 
-    CALL INCREASE
-
-Y3:
-    MOV AX, BX
-    IMUL AX         ; X^2
-    MOV CX, [A]
-    SUB CX, [B]     ; A-B
-    CWD             ; AX -> DX:AX
-    IDIV CX         ; (X^2)/(A-B) 
-    CALL PRINT 
-    CALL INCREASE   
-
-INCREASE:
-    INC BX
-    
 COMPARATOR:  
     CMP BX, SI
     JG END_PROGRAM 
     
     CMP BX, 0h  
     JS Y1           ; BX < 0 
-    JE Y2           ; BX = 0   
-    JG Y3           ; BX > 0 
+    JE Y2           ; BX = 0
+Y3:                 ; BX > 0
+    MOV AX, BX
+    IMUL AX         ; X^2
+    MOV CX, [A]
+    SUB CX, [B]     ; A-B
+    CWD             ; AX -> DX:AX
+    IDIV CX         ; (X^2)/(A-B) 
+    JMP INCREASE       
+Y1:
+    MOV AX, [D]
+    IMUL AX         ; D^2
+    ADD AX, BX      ; D^2+X
+    MOV CX, [B]     
+    IMUL CX         ; (D^2+X)*B 
+    JMP INCREASE
+Y2:
+    MOV AX, [C]      
+    ADD AX, [B]     ; C+B
+    CWD             ; AX -> DX:AX
+    MOV CX, [D]     
+    IDIV CX         ; (C+B)/D
+
+INCREASE:
+    CALL PRINT
     
-PRINT PROC          ; print procedure
+    INC BX
+    JMP COMPARATOR
+
+PRINT PROC
+    PUSH AX
+    PUSH BX
+    
+    MOV [temp_Y], AX
+    
+    ;print "x: "
+    MOV DX, offset msg_x
+    MOV AH, 09h
+    INT 21h
+    
+    ;print x
+    MOV AX, BX
+    CALL PRINT_NUMBER
+    
+    ;print " y: "
+    MOV DX, offset msg_y
+    MOV AH, 09h
+    INT 21h
+    
+    ;print y
+    MOV AX, [temp_Y]
+    CALL PRINT_NUMBER
+    
+    ;print newline
+    MOV DX, offset newline
+    MOV AH, 09h
+    INT 21h
+    
+    POP BX
+    POP AX
+    RET
+PRINT ENDP
+
+PRINT_NUMBER PROC
     PUSH AX
     PUSH BX
     PUSH CX
-    PUSH SI
+    PUSH DX
     
-    TEST AX, AX     ; IF AX < 0 THEN SF=1 
+    TEST AX, AX     ; IF AX < 0 THEN SF=1
     JNS POSITIVE    ; IF SF = 0 (AX >= 0) THEN GOTO POSITIVE
     NEG AX          
     PUSH AX         ; temporary push then pop value of AX
@@ -62,7 +93,7 @@ POSITIVE:
     MOV BX, 10      ; divisor for conversion to the 10-base
 
 PUSH_DIGITS:
-    MOV DX, 0
+    XOR DX, DX      ; clear senior ranks of DX
     DIV BX          ; DX:AX: remainder -> DX; result -> AX
     PUSH DX         ; save symbol in stack
     INC CX
@@ -76,30 +107,27 @@ POP_DIGITS:
     INT 21h         
     LOOP POP_DIGITS ; WHILE CX>0 DO CX-1
     
-    MOV DL, 0Dh     ; newline
-    MOV AH, 02h
-    INT 21h
-    MOV DL, 0Ah
-    MOV AH, 02h
-    INT 21h
-    
-    POP SI          ; restore pushed registers
-    POP CX
+    POP DX
+    POP CX          ; restore pushed registers
     POP BX
     POP AX
     RET
-PRINT ENDP
-                                     
-END_PROGRAM:
-    MOV AH, 4Ch
-    INT 21h  
-    
+PRINT_NUMBER ENDP
+
 A DW 1
 B DW -3
 C DW 12
 D DW 4 
-
 FLOOR DW -3
 CEIL DW 4
+
+temp_Y DW ?
+msg_x DB 'x: $'
+msg_y DB ' y: $'
+newline DB 13,10,'$'
+
+END_PROGRAM:
+    MOV AH, 4Ch
+    INT 21h
 
 END START
